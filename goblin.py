6d62c0d3b1431ec51496180e5dcc64d1205f4880
Andrej Szalma <andrej@fortytwo.studio>
2020-03-03 11:16:29 +0000

    Goblin class working

--- goblin.py
+++ goblin.py
@@ -1,9 +1,62 @@
 #  Author: CS1527 Course Team
 #  Date: 9 January 2020
 #  Version: 1.0
+import random
+from helpers import rock_paper_scissors
 
 
 class Goblin:
-    def __init__(self):
-        return
+    def __init__(self, coord_x, coord_y):
+        self._coordX = coord_x
+        self._coordY = coord_y
 
+    def get_coordinates(self):
+        return self._coordX, self._coordY
+
+    def disappear(self):
+        return False
+
+
+class WealthGoblin(Goblin):
+    def __init__(self, coord_x, coord_y):
+        super(WealthGoblin, self).__init__(coord_x, coord_y)
+        self._coins = random.randint(10, 300)
+        self._probability = round(((400 - self._coins) * random.uniform(0.6, 1.2)) / 4)
+
+    def give_coin(self, hero):
+        probability_list = range(1, self._probability + 1)    # list of numbers stating the probability
+        if random.randint(1, 100) in probability_list:   # random number N = <1;100> check if is in probability_list
+            hero.set_coins(hero.get_coins() + self._coins)
+            print("You have received", self._coins, "coins, your have now", hero.get_coins(), "coins")
+        else:
+            print("You are unlucky, the goblin did not give you any coin.")
+
+
+class HealthGoblin(Goblin):
+    def __init__(self, coord_x, coord_y):
+        super(HealthGoblin, self).__init__(coord_x, coord_y)
+        self._health = random.randint(1,50)
+        self._probability = round((100 - self._health) * random.uniform(0.6, 1.2))
+
+    def give_health(self, hero):
+        probability_list = range(1, self._probability + 1)    # list of numbers stating the probability
+        if random.randint(1, 100) in probability_list:   # random number N = <1;100> check if is in probability_list
+            hero.set_health(hero.get_health() + self._health)
+            print("The goblin has healed by", self._health, "health, your have now", hero.get_health(), "health")
+        else:
+            print("You are unlucky, the goblin did not heal you.")
+
+
+class GamerGoblin(Goblin):
+    def __init__(self, coord_x, coord_y):
+        super(GamerGoblin, self).__init__(coord_x, coord_y)
+        self._health = random.randint(1, 50)
+        self._coins = random.randint(10, 300)
+
+    def play(self, hero):
+        if rock_paper_scissors():
+            print("You lost, you don't get anything from this goblin.")
+        else:
+            hero.set_coins(hero.get_coins() + self._coins)
+            hero.set_health(hero.get_health() + self._health)
+            print("You received", self._coins, "coins and", self._health, "health, you have", hero.get_coins(), "coins and", hero.get_health(), "health")
